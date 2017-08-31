67928c948d0dd0423c54f3dce31dfe741d6b6af0
Jst <jkavaliauskas@gmail.com>
2017-08-31 21:21:14 +0300

    ivadas

---
+++ receptas.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Receptas</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+
+$recept = ["Jautiena" => 250, "Pupeles" => 500, "Vanduo" => 500, "Pomidorai" => 300, "Paprika" => 200];
+
+print_r($recept);
+
+$suma= 0;
+
+foreach ($recept as $dalis => $kiekis) {
+	echo $dalis . ": " .  $kiekis . "<br />";
+ }  $suma += $kiekis;
+
+
+echo $suma
+
+Checkwater($recept)
+?>
